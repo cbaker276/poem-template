abbff4d651e8497dd6c48373c7fdc3a3dca91a6c
cbaker276 <connorb276@gmail.com>
2025-03-02 19:56:58 -0500

    Icons and backgrounds fixed
    this change added other social media icons and also adjusted background color to black

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,18 +1,33 @@
 import TwitterIcon from '@/components/icons/TwitterIcon'
 import GithubIcon from '@/components/icons/GithubIcon'
+import InstagramIcon from '@/components/icons/InstagramIcon'
+import XIcon from '@/components/icons/XIcon'
+import TicTokIcon from '@/components/icons/TicTokIcon'
+import YouTubeIcon from '@/components/icons/YouTubeIcon'
 
 // ADD YOUR SOCIAL NETWORKS HERE
 export const SOCIALNETWORKS = [
 	{
-		name: 'Github',
-		url: 'https://github.com/danielcgilibert/blog-template',
-		icon: GithubIcon
+		name: 'Instagram',
+		url: 'https://www.instagram.com/blnkspacepoetry/',
+		icon: InstagramIcon
 	},
 
 	{
-		name: 'Twitter',
+		name: 'X',
 		url: 'https://x.com/BlnkSpacePoetry',
-		icon: TwitterIcon
-	}
+		icon: XIcon
+	},
 
+	{
+		name: 'TicTok',
+		url: 'https://www.tiktok.com/@blnkspacepoetry',
+		icon: TicTokIcon
+	},
+
+	{
+		name: 'YouTube',
+		url: 'https://www.youtube.com/@blankspacepoetry',
+		icon: YouTubeIcon
+	}
 ] as const
