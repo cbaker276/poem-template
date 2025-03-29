cf9f25ddb2bc832e657dd9e35047d5f726c8607c
cbaker276 <connorb276@gmail.com>
2025-03-29 15:45:40 -0400

    Updated favicon

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -13,9 +13,9 @@ export const siteConfig: SiteConfig = {
 	site: 'https://blankspacepoetry.com', // Write here your website url
 	author: 'Connor', // Site author
 	title: 'Blank Space Poetry', // Site title.
-	description: 'This site was made by me to share with the world, thanks!', // Description to display in the meta tags
+	description: 'Fill your blank spaces with poetry and find the color that fits you best.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
-	paginationSize: 6 // Number of posts per page
+	paginationSize: 12 // Number of posts per page
 }
