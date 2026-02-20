9d9b7e7c1583b3375430664f57da50f17a65792c
cbaker276 <connorb276@gmail.com>
2026-02-20 17:39:28 -0500

    Green Stage

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,2 @@
 // List of categories for blog posts
-export const CATEGORIES = ['White', 'Yellow', 'Blue', 'Coming Soon', 'Black', 'About Me'] as const
+export const CATEGORIES = ['White', 'Yellow', 'Blue', 'Green', 'Black', 'About Me'] as const
