adc8bccfce378d7b900c7260b145cc06b7cf663d
cbaker276 <connorb276@gmail.com>
2026-04-25 19:23:58 -0400

    Short stories and Bird Feeder

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,10 @@
 // List of categories for blog posts
-export const CATEGORIES = ['White', 'Yellow', 'Blue', 'Green', 'Black', 'About Me'] as const
+export const CATEGORIES = [
+	'White',
+	'Yellow',
+	'Blue',
+	'Green',
+	'Black',
+	'Short Stories',
+	'About Me'
+] as const
